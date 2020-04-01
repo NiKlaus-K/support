d0e4a5d97217926d0d3e147bd2ef0864311fa4ea
Noober <541657414@qq.com>
2020-04-01 13:31:50 +0800

    云函数尝试

---
+++ cloudfunctions/commentsCount/index.js
@@ -0,0 +1,23 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database()
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  try {
+  return await db.collection('comments')
+    .where({
+      workId: ''
+    }).count()
+  } catch(e){
+    console.error(e)
+  }
+  // return {
+  //   event,
+  //   openid: wxContext.OPENID,
+  //   appid: wxContext.APPID,
+  //   unionid: wxContext.UNIONID,
+  // }
+}
