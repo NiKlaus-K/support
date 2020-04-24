5a0f224b3ddfb453c89cdd4e532760e3cb9b8919
Noober <541657414@qq.com>
2020-04-24 17:10:49 +0800

    用户界面调整

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -12,55 +12,22 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取用户信息
+    this.getUserInfo();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取用户信息
+  getUserInfo: function() {
+    let userInfo = wx.getStorageSync('userInfo')
+    if(userInfo){
+      this.setData({
+        userInfo: userInfo
+      })
+    }
+    let openid = wx.getStorageSync('openid').openid
+    if(openid){
+      this.setData({
+        openid: openid
+      })
+    }
   }
 })
